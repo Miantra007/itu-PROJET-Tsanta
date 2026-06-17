4291c9aedfa6289e0aeaccf3c0b51c5b28575413
RAKOTONIAINA Miantra Vahatrasoa <rakotoniainami5@gmail.com>
2026-06-17 10:08:17 +0300

    form achat

--- app/Views/Achats.php
+++ app/Views/Achats.php
@@ -1,17 +1,46 @@
+<?php $caisse = session()->get('caisse_active'); ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Achat</title>
 </head>
 
 <body>
-    <h1>Faites vos achats</h1>
-    <form action="">
 
-    </form>
+    <div>
+        <h2>Faites vos achats</h2>
+
+        <p>
+            Caisse active : <?= $caisse['num'] ?>
+        </p>
+
+        <form action="<?= base_url('/achat/ajouter') ?>" method="post">
+            <?= csrf_field() ?>
+
+            <input type="hidden" name="id_caisse" value="<?= $caisse['id'] ?>">
+
+            <p>Produits :
+                <select name="id_produit" required>
+                    <option value="">selectionner</option>
+                    <?php foreach ($produits as $produit): ?>
+                        <option value="<?= $produit['id_produit'] ?>">
+                            <?= $produit['designation'] ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </p>
+
+            <p>Quantite :
+                <input type="number" name="quantite">
+            </p>
+
+            <button type="submit">Valider</button>
+        </form>
+    </div>
+
 </body>
 
 </html>
