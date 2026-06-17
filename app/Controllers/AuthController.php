83c709b8f22e28d3e9ec97f618ac6594307f1e35
RAKOTONIAINA Miantra Vahatrasoa <rakotoniainami5@gmail.com>
2026-06-17 09:45:28 +0300

    config login

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ClientModel;
+
+class AuthController extends BaseController
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function login()
+    {
+        $session = session();
+        $model = new ClientModel();
+
+        $email = $this->request->getPost('email');
+        $mdp = $this->request->getPost('mdp');
+
+        if (empty($email) || empty($mdp)) {
+            return redirect()->back()->with('error', 'Tous les champs sont obligatoires');
+        }
+        $user = $model
+            ->select('id_client, nom, email, mot_de_passe')
+            ->where('email', $email)
+            ->first();
+            
+        if (!$user) {
+            return redirect()->back()->with('error', 'Email incorrect');
+        }
+
+        if ($mdp != $user['mot_de_passe']) {
+            return redirect()->back()->with('error', 'Mot de passe incorrect');
+        }
+
+        $session->set([
+            'id_client' => $user['id_client'],
+            'nom' => $user['nom'],
+            'email' => $user['email'],
+            'isLoggedIn' => true
+        ]);
+
+        return redirect()->to('/dashboard');
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('/login');
+    }
+}
--- data.sql
+++ data.sql
@@ -1,12 +1,25 @@
 -- Insertion des 5 produits requis (avec les exemples du TD)
-INSERT INTO produit (designation, prix_unitaire, quantite_stock) VALUES 
-('Biscuit', 1000.0, 150),
-('Pain', 400.0, 80),
-('Chocolat', 2500.0, 50),
-('Jus de fruits', 4500.0, 30),
-('Eau minérale', 1200.0, 200);
+INSERT INTO
+    produit (designation, prix_unitaire, quantite_stock)
+VALUES
+    ('Biscuit', 1000.0, 150),
+    ('Pain', 400.0, 80),
+    ('Chocolat', 2500.0, 50),
+    ('Jus de fruits', 4500.0, 30),
+    ('Eau minérale', 1200.0, 200);
 
 -- Insertion des 2 caisses requises
-INSERT INTO caisse (num_caisse) VALUES 
-('Caisse N°1'),
-('Caisse N°2');
+INSERT INTO
+    caisse (num_caisse)
+VALUES
+    ('Caisse N°1'),
+    ('Caisse N°2');
+
+INSERT INTO
+    client (nom, email, mot_de_passe)
+VALUES
+    ('Rakoto', 'rakoto@gmail.com', 'pass123'),
+    ('Rabe', 'rabe@yahoo.com', 'rabe456'),
+    ('Lova', 'lova@gmail.com', 'lova789'),
+    ('Nina', 'nina@hotmail.com', 'nina321'),
+    ('Tiana', 'tiana@gmail.com', 'tiana000');
